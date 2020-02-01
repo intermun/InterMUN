80bae82e516a01fbda73f9b6f7fa717cdbe214c3
radupasparuga <radupasparuga25@gmail.com>
2020-02-01 17:46:19 +0200

    Added gitignore for sendgrid, functions work now

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,12 +2,7 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp()
-const db = admin.firestore();
-
-exports.test = functions.https.onRequest(async (req, res) => {
-  await db.collection("delegates").add({ test: "yes" });
-  res.sendStatus(200);
-});
+//const db = admin.firestore();
 
 exports.meme = functions.firestore.document('delegates/{delegateId}').onCreate((snap, context) => {
   console.log(snap.data())
