6acd3ac8465cd68b86159971cdf27aae59aa5734
radupasparuga <radupasparuga25@gmail.com>
2020-02-01 19:23:25 +0200

    Email works!

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,24 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as sgMail from '@sendgrid/mail';
 
 admin.initializeApp()
-//const db = admin.firestore();
+sgMail.setApiKey(functions.config().sendgrid.key)
 
 exports.meme = functions.firestore
     .document('delegates/{delegateId}')
     .onCreate((snap, context) => {
-      console.log(snap.data())
+      const data: any = snap.data();
+      const msg = {
+        to: data.email,
+        from: functions.config().sendgrid.from,
+        templateId: functions.config().sendgrid.template,
+        dynamic_template_data: {
+          committee: data.committee,
+          country: data.country,
+          fullName: data.fullName,
+          highSchool: data.highSchool,
+        },
+      };
+      sgMail.send(msg)
     });
