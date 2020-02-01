c43ae6b2127dc3cc809d78912b8ed22d127d16d9
radupasparuga <radupasparuga25@gmail.com>
2020-02-01 18:22:32 +0200

    You should be working, but you don't

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,8 @@ import * as admin from 'firebase-admin';
 admin.initializeApp()
 //const db = admin.firestore();
 
-exports.meme = functions.firestore.document('delegates/{delegateId}').onCreate((snap, context) => {
-  console.log(snap.data())
-})
+exports.meme = functions.firestore
+    .document('delegates/{delegateId}')
+    .onCreate((snap, context) => {
+      console.log(snap.data())
+    });
